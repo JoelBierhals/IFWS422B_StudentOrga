d05d8496db2cfa5726d17d423a645812d82c6ff7
JoelBierhals <128902338+JoelBierhals@users.noreply.github.com>
2023-06-23 12:35:21 +0200

    Lecturer extends Person

--- src/controller/LecturerCtr.java
+++ src/controller/LecturerCtr.java
@@ -1,10 +1,20 @@
 package controller;
 
 import model.Lecturer;
+import model.Person;
 
 public class LecturerCtr {
     public static void main(String[] args) throws Exception {
 
-        Lecturer lecturer1 = new Lecturer("Iris", "Handheide");
+        Lecturer lect1 = new Lecturer("Iris", "Handheide");
+        System.out.println(lect1.info());
+
+        Lecturer lect3 = new Lecturer();
+        lect3.newInputDialogue();
+
+        System.out.println(lect3.info());
+
+        Person.newInputDialogue(lect3);
+        System.out.println(lect3.info());
     }
 }
--- src/model/Lecturer.java
+++ src/model/Lecturer.java
@@ -1,51 +1,19 @@
 package model;
 
+// Lecturere im Project StudentOrg
+//  @autor Joel Bierhals
+
 public class Lecturer extends Person {
-    private String firstName;
-    private String lastName;
-    private int birth;
 
     public Lecturer() {
-
     }
 
     public Lecturer(String firstName, String lastName) {
-        // this();
-        this.firstName = firstName;
-        this.lastName = lastName;
+       super(firstName, lastName);
     }
 
     public Lecturer(String firstName, String lastName, int birth) {
-        this(firstName, lastName);
-        this.birth = birth;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getBirth() {
-        return birth;
-    }
-
-    public void setBirth(int birth) {
-        this.birth = birth;
-    }
-
-    public static void main(String[] args) throws Exception {
-
+        super(firstName, lastName, birth);
     }
     
 }
